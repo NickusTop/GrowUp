530148dbf873d98fc1b77da1f4768d89ac2922f1
NickusTop <148118087+NickusTop@users.noreply.github.com>
2025-11-23 13:42:20 +0200

    GrowUp

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@ window.addEventListener('scroll', () => {
     header.classList.remove('active');
   }
 });
+
+const menuItems = document.querySelectorAll('.header-li');
+
+menuItems.forEach(li => {
+  const submenu = li.querySelector('.header-ul-menu'); // подменю конкретного li
+  if (!submenu) return; // если подменю нет, пропускаем
+
+  let timeout = null;
+
+  const openMenu = () => {
+    clearTimeout(timeout);
+    submenu.classList.add('open');
+  };
+
+  const closeMenu = () => {
+    timeout = setTimeout(() => {
+      submenu.classList.remove('open');
+    }, 200);
+  };
+
+  li.addEventListener('mouseenter', openMenu);
+  li.addEventListener('mouseleave', closeMenu);
+
+  submenu.addEventListener('mouseenter', openMenu);
+  submenu.addEventListener('mouseleave', closeMenu);
+});
+
